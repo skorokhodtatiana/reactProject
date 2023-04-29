02eafe0597ad815d84e43f32f6082c9f6df292bb
tskorohod <tskorohod@lmteam.by>
2023-04-29 08:57:02 +0300

    in process modal window

--- myapp/src/components/Modal/Modal.jsx
+++ myapp/src/components/Modal/Modal.jsx
@@ -3,7 +3,11 @@ import styles from './Modal.module.scss';
 const Modal = () => {
 	return (
 		<>
-			<div className={ styles.container }>Hi</div>
+			<div className={ styles.container }>
+				<div className={ styles.body }>
+					<div className={ styles.close }></div>
+				</div>
+			</div>
 		</>
 	)
 }
